ad9ddce9e9efe60ff07946f7e5cca6ec5c872065
Colin Franklin <colinfranklin@Colins-MacBook-Air.local>
2024-09-09 20:37:16 -0700

    first commit

---
+++ calculator.py
@@ -0,0 +1,22 @@
+print("Choose an operation: ")
+print("a) Addition")
+print("b) Subtraction")
+print ("c) Multipication")
+print("d) Division")
+operation = input()
+print("Type your first number: ")
+first_num = int(input())
+print("Type your second number: ")
+second_num = int(input())
+if operation== "a":
+    answer = first_num + second_num
+elif operation== "b":
+    answer = first_num - second_num
+elif operation== "c":
+    answer = first_num * second_num
+elif operation== "d":
+    answer = first_num / second_num
+else:
+    print("Error, please pick the correct letter")
+
+print(answer)
